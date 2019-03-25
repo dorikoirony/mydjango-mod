010ac345aee0d0e8c63448381af0ef2214acfad4
nanoRIPE <1033188854@qq.com>
2019-03-25 19:42:42 +0800

    增加图片功能

--- gallery/models.py
+++ gallery/models.py
@@ -4,6 +4,13 @@ from django.db import models
 class Gallery(models.Model):
     """Model definition for MODELNAME."""
     objects=models.Manager()
-    description = models.CharField(max_length=50)
+    description = models.CharField(default='请输入简介',max_length=100)
+    image = models.ImageField(default='default.png',upload_to='images/')
+    title = models.CharField(default='作品标题',max_length=50)
+    
+    def __str__(self):              #用于修改标题
+        return self.title               
+
+
  
 
