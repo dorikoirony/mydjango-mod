c3c67e0ce04c8b7f77e44e1d49e9f6babcc6f198
nanoRIPE <1033188854@qq.com>
2019-03-25 18:49:09 +0800

    模板使用成功

--- gallery/models.py
+++ gallery/models.py
@@ -3,7 +3,7 @@ from django.db import models
 # Create your models here.
 class Gallery(models.Model):
     """Model definition for MODELNAME."""
-    object=models.Manager()
-    description=models.CharField(max_lenth=100)
+    objects=models.Manager()
+    description = models.CharField(max_length=50)
  
 
