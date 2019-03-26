143726a8a8183d5bd18f5c7ec756fae5774df7cc
nanoRIPE <1033188854@qq.com>
2019-03-26 15:10:09 +0800

    博客添加

---
+++ blog/models.py
@@ -0,0 +1,15 @@
+from django.db import models
+
+# Create your models here.
+class Blog(models.Model):
+    """Model definition for MODELNAME."""
+
+    # TODO: Define fields here
+    objects=models.Manager()
+    title = models.CharField(default='文章标题', max_length=50)
+    date = models.DateField()
+    image = models.ImageField(default='default.png',upload_to='images/')
+    text = models.TextField(default='文章正文')
+    def __str__(self):
+        """Unicode representation of MODELNAME."""
+        return self.title
